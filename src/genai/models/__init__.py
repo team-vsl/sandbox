9c98e68cb3508156498a45fca26053ae399d9395
NghiemCanCode <nghiemgankteam@gmail.com>
2025-07-16 09:21:03 +0700

    feat(bedrock): update defintions prompt template

--- src/genai/models/__init__.py
+++ src/genai/models/__init__.py
@@ -1,9 +1,9 @@
 from .info import Info
-from .server import Server
+from .server import Server, S3Server, RedshiftServer
 from .terms import Terms
 from .data_models import Models
 from .definitions import DefinitionObject
 from .service_levels import ServiceLevels
 from .data_field import DataField, Quality
 
-__all__ = [Info, Server, Terms, Models, DefinitionObject, ServiceLevels, DataField, Quality]
+__all__ = [Info, Server, Terms, Models, DefinitionObject, ServiceLevels, DataField, Quality, S3Server, RedshiftServer]
