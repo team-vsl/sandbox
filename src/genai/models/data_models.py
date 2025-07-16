3a8d0a6ac59fe0f73b85161b070e2dff212f0249
NghiemCanCode <nghiemgankteam@gmail.com>
2025-07-16 16:00:49 +0700

    feat(bedrock): add agent to generate model object

--- src/genai/models/data_models.py
+++ src/genai/models/data_models.py
@@ -3,10 +3,6 @@ from typing import Optional, Dict, List, Any
 from .data_field import DataField, Quality
 
 
-class Config(BaseModel):
-    pass
-
-
 class Models(BaseModel):
     type: str = Field(default="table", description="The type of the model. E.g., table, view, object.")
     description: Optional[str] = Field(default=None, description="A description of the data model.")
@@ -16,4 +12,3 @@ class Models(BaseModel):
     quality: Optional[List[Quality]] = None
     examples: Optional[List[Any]] = None
     additionalFields: Optional[bool] = Field(default=False)
-    config: Optional[Config] = None
